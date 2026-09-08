03faac87a025acbe62840bedbe69000f1c0bb209
ZhaoCang-QWQ <zhaocang-qwq@users.noreply.github.com>
2026-09-08 10:13:48 +0800

    v0.7.2：主动提起支持多群随机挑选（原来只取列表第一个）

--- plugin.py
+++ plugin.py
@@ -23,7 +23,7 @@ from typing import Any, Literal
 from maibot_sdk import Command, Field, MaiBotPlugin, PluginConfigBase, Tool
 from maibot_sdk.types import ToolParameterInfo, ToolParamType
 
-PLUGIN_VERSION = "0.7.1"
+PLUGIN_VERSION = "0.7.2"
 SUPPORTED_CONFIG_VERSION = PLUGIN_VERSION
 
 
